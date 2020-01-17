4b3b340208980dd8e7ba0e2939412bccacb3116f
Rafael Faria <rafaelsanfaria@gmail.com>
2020-01-17 11:53:27 -0300

    added ejs files and models

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const session = require('express-session')
 
 mongoose.Promise = global.Promise
 const mongo = 'mongodb://localhost/socketio-chat'
@@ -9,9 +10,34 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.set('view engine', 'ejs')
+app.use(session({
+  secret: 'socketio',
+  resave: true,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 60 * 10 * 1000
+  }
+}))
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 
 app.get('/', (req, res) => res.render('home'))
+app.post('/', (req, res) => {
+  req.session.user = {
+    name: req.body.name
+  }
+  res.redirect('/room')
+})
+
+app.get('/room', (req, res) => {
+  if (!req.session.user) {
+    res.redirect('/')
+  } else {
+    res.render('room', { name: req.session.user.name })
+  }
+})
 
 mongoose.connect(mongo, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
